9db4addd722cdfccddd3c49ace0921cd6882fdbf
junecho67 <yejune.cho@tufts.edu>
2024-10-30 15:39:00 -0400

    Update survey.js

--- survey.js
+++ survey.js
@@ -1,95 +1,84 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const survey = document.querySelector("#survey");
 
-document.addEventListener("readystatechange", () => {
-    let survey = document.querySelector("#survey");
-    let other_diet = survey.querySelector("#other-diet");
-    let other_diet_text = survey.querySelector("#other-diet-text");
-    other_diet.addEventListener("click", (evt) => {
-
+    // Enable/disable "Other Diet" text input
+    const other_diet = survey.querySelector("#other-diet");
+    const other_diet_text = survey.querySelector("#other-diet-text");
+    other_diet.addEventListener("click", () => {
         other_diet_text.disabled = false;
-    })
+    });
 
     survey.querySelectorAll('input[name="dietary restrictions"]').forEach(radio_btn => {
         radio_btn.addEventListener("click", () => {
             if (radio_btn !== other_diet) {
-                other_diet_text.disabled = true; // Disable text input if another option is selected
+                other_diet_text.disabled = true;
             }
         });
     });
 
+    // Enable/disable "Other Dietary Goals" text input
+    const other_dietary_goals = survey.querySelector("#other-dietary-goals");
+    const other_dietary_goals_text = survey.querySelector("#other-dietary-goals-text");
+    other_dietary_goals.addEventListener("click", () => {
+        other_dietary_goals_text.disabled = !other_dietary_goals.checked;
+    });
 
-    let other_dietary_goals = survey.querySelector("#other-dietary-goals");
-    let other_dietary_goals_text = survey.querySelector("#other-dietary-goals-text");
-    other_dietary_goals.addEventListener("click", (evt) => {
-        if (other_dietary_goals.checked) {
-            other_dietary_goals_text.disabled = false;
-        }
-        else {
-            other_dietary_goals_text.disabled = true;
-        }
-    })
+    // Enable/disable allergies text input
+    const allergies = survey.querySelector("#yes-allergies");
+    const allergies_text = survey.querySelector("#yes-allergies-text");
+    const no_allergies = survey.querySelector("#no-allergies");
 
-    survey.querySelector("#no-allergies").addEventListener("click", (evt) => {
-        allergies_text.disabled = true;
-    })
-
-    let allergies = survey.querySelector("#yes-allergies");
-    let allergies_text = survey.querySelector("#yes-allergies-text");
-    allergies.addEventListener("click", (evt) => {
+    allergies.addEventListener("click", () => {
         allergies_text.disabled = false;
-    })
+    });
 
+    no_allergies.addEventListener("click", () => {
+        allergies_text.disabled = true;
+    });
 
-    let submit_btn = document.querySelector("#submit-btn");
-    submit_btn.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        let diet_error = survey.querySelector("#diet-error");
-        let selected_diet = survey.querySelector('input[name="dietary restrictions"]:checked');
+    // Submit button click
+    const submit_btn = document.querySelector("#submit-btn");
+    submit_btn.addEventListener("click", () => {
+        const diet_error = survey.querySelector("#diet-error");
+        const unwanted_foods_error = survey.querySelector("#unwanted-foods-error");
+        const dietary_goals_error = survey.querySelector("#dietary-goals-error");
+
+        const selected_diet = survey.querySelector('input[name="dietary restrictions"]:checked');
+        const selected_allergy = survey.querySelector('input[name="allergies"]:checked');
+        const selected_goals = survey.querySelector('input[name="dietary-goals"]:checked');
+
+        diet_error.innerHTML = "";
+        unwanted_foods_error.innerHTML = "";
+        dietary_goals_error.innerHTML = "";
+
+        // Validate dietary restrictions
         if (selected_diet) {
-            if ((selected_diet.value === "other-diet") & (survey.querySelector("#other-diet-text").value === "")) {
+            if (selected_diet.value === "other-diet" && other_diet_text.value.trim() === "") {
                 diet_error.innerHTML = "Please enter a diet you want to follow";
             }
-            else {
-                diet_error.innerHTML = "";
-            }
-        }
-        else {
-            diet_error.innerHTML = "Please select a choice";
+        } else {
+            diet_error.innerHTML = "Please select a dietary restriction choice";
         }
 
-        let selected_allergy = survey.querySelector('input[name="allergies"]:checked');
-        let unwanted_foods_error = survey.querySelector("#unwanted-foods-error");
+        // Validate allergies
         if (selected_allergy) {
-            if ((selected_allergy.value === "yes-allergies") & (survey.querySelector("#yes-allergies-text").value === "")) {
+            if (selected_allergy.value === "yes-allergies" && allergies_text.value.trim() === "") {
                 unwanted_foods_error.innerHTML = "Please enter any allergies you have";
-
             }
-            else {
-                unwanted_foods_error.innerHTML = "";
-            }
-        }
-        else {
-            unwanted_foods_error.innerHTML = "Please select a choice";
+        } else {
+            unwanted_foods_error.innerHTML = "Please select a choice for allergies";
         }
 
-        let selected_goals = survey.querySelector('input[name="dietary-goals"]:checked');
-        let dietary_goals_error = survey.querySelector("#dietary-goals-error");
+        // Validate dietary goals
         if (selected_goals) {
-            if (selected_goals.value === "other-dietary-goals" & (survey.querySelector("#other-dietary-goals-text").value === "")) {
+            if (selected_goals.value === "other-dietary-goals" && other_dietary_goals_text.value.trim() === "") {
                 dietary_goals_error.innerHTML = "Please enter a dietary goal";
             }
-            else {
-                console.log(selected_goals.value);
-                dietary_goals_error.innerHTML = "";
-            }
         }
 
-        if ((diet_error.innerHTML === "") & (unwanted_foods_error.innerHTML === "")) {
-            if (dietary_goals_error.innerHTML === "") {
-                window.location.href = "results.html";
-            }
+        // Redirect to results.html if all validations pass
+        if (diet_error.innerHTML === "" && unwanted_foods_error.innerHTML === "" && dietary_goals_error.innerHTML === "") {
+            window.location.href = "results.html";
         }
-
-
-
-    })
-})
+    });
+});
